41dce7874d774abdfdf47ce9dc3018d5c81caf90
cmihiranga099 <chamod.bsc.se02@edu.lnbti.lk>
2026-09-15 15:35:23 +0530

    Update .gitignore to exclude sensitive files, modify Program.cs to use MySQL for DbContext, and remove initial migration files.

--- NovaMartPOS.API/Program.cs
+++ NovaMartPOS.API/Program.cs
@@ -16,8 +16,13 @@ namespace NovaMartPOS.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString)
+                ));
 
             var app = builder.Build();
 
