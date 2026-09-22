aa00242c89937ef2a462310eb476dca18d3161c1
cmihiranga099 <chamod.bsc.se02@edu.lnbti.lk>
2026-09-22 10:35:37 +0530

    Add PinHash property to user model in AppDbContextModelSnapshot for enhanced security in authentication processes.

--- NovaMartPOS.Infrastructure/Migrations/20260919120000_AddUserPinHash.Designer.cs
+++ NovaMartPOS.Infrastructure/Migrations/20260919120000_AddUserPinHash.Designer.cs
@@ -1,8 +1,9 @@
-﻿
+
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NovaMartPOS.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using NovaMartPOS.Infrastructure.Persistence;
 namespace NovaMartPOS.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260919120000_AddUserPinHash")]
+    partial class AddUserPinHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
